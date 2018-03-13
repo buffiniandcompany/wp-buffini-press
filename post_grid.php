24443527e7184db869855ffdce7cbbd539acc728
pabrigo <pip.abrigo@gmail.com>
2018-03-13 10:33:31 -0700

    press 2

--- post_grid.php
+++ post_grid.php
@@ -1,6 +1,8 @@
 		<?php
-		postFeatured('buffini-featured', '');
-		postCategory( 'build-and-lead', 'Build and Lead' );
-		postCategory( 'generate-more-leads', 'Generate More Leads' );
-		postCategory( 'get-motivated', 'Get Motivated' );
+		//postFeatured('buffini-featured', '');
+		postCategory( '2018', '2018' );
+		postCategory( '2017', '2017' );
+		postCategory( '2016', '2016' );
+		postCategory( '2015', '2015' );
+		postCategory( '2014', '2014' );
 ?>
