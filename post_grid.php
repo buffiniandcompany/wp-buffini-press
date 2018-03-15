4b96e745e0d7940fa624a53c26d477dc5601ca9c
buffini-pipa <pipa@buffiniandcompany.com>
2018-03-15 10:57:56 -0700

    press child 1

--- post_grid.php
+++ post_grid.php
@@ -1,8 +1,7 @@
 		<?php
-		//postFeatured('buffini-featured', '');
+		postFeatured('buffini-featured', '');
 		postCategory( '2018', '2018' );
 		postCategory( '2017', '2017' );
 		postCategory( '2016', '2016' );
 		postCategory( '2015', '2015' );
-		postCategory( '2014', '2014' );
 ?>
